efde902fc347bef83ec1611cd1b26b1e935133ac
Dariush Komeili <dkomeili@proton.me>
2026-01-03 22:25:34 +0000

    Update database backup script to define destination variable and ensure cleanup

--- backup-scripts/tuwunel/tuwunel-db-backup.sh
+++ backup-scripts/tuwunel/tuwunel-db-backup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+DEST="/mnt/data/backup-dumps/tuwunel-nomedia"
+
 echo "attempting to trigger database backup internally..."
+
+rm -rf "$DEST"
 
 systemctl --user kill --signal=SIGUSR2 tuwunel.service
 
